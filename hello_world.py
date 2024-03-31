f1d50d5f71e197ea7a1c47b1743517a9c1e5a950
kirillsidorko <157414242+kirillsidorko@users.noreply.github.com>
2024-03-31 13:36:11 -0500

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,11 @@
+# Your Name: [Your Name Here]
+# Class: [Your Class Here]
+# This program prints a personalized Hello World message.
+
+# The first and last name, followed by a possessive curly apostrophe and "Hello World"
+first_name = "Eric"
+last_name = "Pogue"
+message = f"{first_name} {last_name}’s Hello World"
+
+print(message)
 
